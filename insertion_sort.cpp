f1b1c6e9ac593e984245a913e5710db5b96afe6c
Hiron <faisal.hiron2603@gmail.com>
2025-08-04 03:36:06 +0600

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+using namespace std; 
+
+
+void insertionSort(int arr[], int n)
+{ 
+    for (int i = 1; i < n; i++) { 
+        int key = arr[i]; // Current element to be inserted 
+        int j = i - 1; 
+        // Move elements of arr[0..i-1] that are greater than key 
+        // to one position ahead of their current position 
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j--;
+        }
+        arr[j + 1] = key; // Insert the key in the correct position} 
+    }
+}
+
+ 
+int main() { 
+    int arr[] = {5, 1, 4, 2, 8}; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+ 
+    cout << "Initial Array: "; 
+    for (int i = 0; i < n; i++)  
+    cout << arr[i] << " ";
+
+    cout << endl; 
+
+
+    insertionSort(arr, n); 
+
+    cout << "Sorted Array: "; 
+    for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+    cout << endl; 
+
+return 0;
+} 

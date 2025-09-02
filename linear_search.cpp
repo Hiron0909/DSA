e43563b45e249266b70eeeb7005ca5c04a53aa02
Hiron <faisal.hiron2603@gmail.com>
2025-09-02 22:23:07 +0600

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -13,18 +13,26 @@ int linearSearch(int arr[], int n, int key)
    return -1;  // Element not found 
 }
 
-
 int main() 
 { 
-  int arr[] = {10, 20, 30, 40, 50}; 
-  int n = sizeof(arr) / sizeof(arr[0]); 
-  int key = 30; 
+ int n;
+  cout << "Enter the size of the arary: ";
+  cin >> n;
+  int arr[n];
+  cout << "Enter elements in array : ";
+
+  for(int i = 0; i<n; i++)
+  {
+    cin >> arr[i];
+  }
+  int key;
+  cout << "Enter the key element which you want to find : ";
+  cin >> key;
   int result = linearSearch(arr, n, key); 
   if (result != -1) 
   cout << "Element found at index " << result << endl; 
   else 
   cout << "Element not found" << endl; 
 
-  
   return 0; 
 }
